e18b790b2af80c3a376cd00954d504adc420f878
enam007 <aamireee007@gmail.com>
2023-11-25 21:59:59 +0530

    adding utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 
+const port = process.env.PORT  || 8000
 
-connectDB()
+try{
+    await connectDB()
+    app.listen(port,()=>{
+        console.log(`Server running on ${port}`)
+    })
+    app.on('error',(err)=>{
+        console.error('EXPRESS SERVER ERROR !!! ',err)
+    })
+} catch(error){
+    console.log('MONGODB CONNECTION FAILED !!! ',error);
+}
