1e1a7cca61905fe4a206787efde711d004ef1012
enam007 <aamireee007@gmail.com>
2023-11-27 14:08:34 +0530

    adding multer config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import connectDB from "./db/index.js";
-import {app} from "./app.js"
+import { app } from "./app.js";
 
+const port = process.env.PORT || 8000;
 
-const port = process.env.PORT  || 8000
-
-try{
-    await connectDB()
-    app.listen(port,()=>{
-        console.log(`Server running on ${port}`)
-    })
-    app.on('error',(err)=>{
-        console.error('EXPRESS SERVER ERROR !!! ',err)
-    })
-} catch(error){
-    console.log('MONGODB CONNECTION FAILED !!! ',error);
+try {
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+  });
+  app.on("error", (err) => {
+    console.error("EXPRESS SERVER ERROR !!! ", err);
+  });
+} catch (error) {
+  console.log("MONGODB CONNECTION FAILED !!! ", error);
 }
